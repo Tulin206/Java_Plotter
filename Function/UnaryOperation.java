675288cf8871daffaee9f1088ff9ad86f1aae52c
Israt Jahan Tulin <61561356+Tulin206@users.noreply.github.com>
2022-11-10 00:19:26 +0100

    Add files via upload

---
+++ Function/UnaryOperation.java
@@ -0,0 +1,13 @@
+package Plotter.Function;
+
+public abstract class UnaryOperation extends Function {
+	
+	protected Function operator;
+	
+	public UnaryOperation(Function operator) {
+		this.operator = operator;
+		// TODO
+		// throw new UnsupportedOperationException();
+	}
+	
+}
---
+++ Parser/Scanner/Scanner.java
@@ -0,0 +1,94 @@
+package Plotter.Parser.Scanner;
+
+public class Scanner {
+	
+	private Token buffer;
+	private String string;
+	
+	public Scanner(String input) {
+		buffer = null;
+		string = input;
+	}
+	
+	public Token nextToken() {
+		if (buffer != null) {
+			Token tmp = buffer;
+			buffer = null;
+			return tmp;
+		}
+		
+		// ensure first character is no whitespace character
+		string = string.trim();
+		
+		if (string.length() == 0) {
+			return new Token(TokenType.EOF, "");
+		}
+		
+		// handle operators and symbols
+		switch (string.charAt(0)) {
+		case '+':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.PLUS_OP, "+");
+		case '-':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.MINUS_OP, "-");
+		case '*':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.MULT_OP, "*");
+		case '/': case ':':
+			String tmp = string.substring(0, 1);
+			string = string.substring(1, string.length());
+			return new Token(TokenType.DIV_OP, tmp);
+		case '^':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.EXP_OP, "^");
+		case '(':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.LEFT_PAR, "(");
+		case ')':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.RIGHT_PAR, ")");
+		case '|':
+			string = string.substring(1, string.length());
+			return new Token(TokenType.VERT, "|");
+		}
+		
+		// handle numbers
+		if (Character.isDigit(string.charAt(0))) {
+			int length = 1;
+			while (length < string.length() && Character.isDigit(string.charAt(length))) {
+				length++;
+			}
+			if (length < string.length() && string.charAt(length) == '.') {
+				length++;
+				while (length < string.length() && Character.isDigit(string.charAt(length))) {
+					length++;
+				}
+			}
+			String tmp = string.substring(0, length);
+			string = string.substring(length, string.length());
+			return new Token(TokenType.NUMBER, tmp);
+		}
+		
+		// handle identifiers
+		if (Character.isLowerCase(string.charAt(0))) {
+			int length = 1;
+			while (length < string.length() && Character.isLowerCase(string.charAt(length))) {
+				length++;
+			}
+			String tmp = string.substring(0, length);
+			string = string.substring(length, string.length());
+			return new Token(TokenType.IDENTIFIER, tmp);
+		}
+				
+		throw new UnsupportedOperationException();
+	}
+	
+	public void pushBack(Token token) {
+		if (buffer != null) {
+			throw new UnsupportedOperationException();
+		}
+		buffer = token;
+	}
+	
+}
